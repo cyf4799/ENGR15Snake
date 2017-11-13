f19cd393cd22451dcb19bb8651023c6b55cf071f
Yi Fei Cheng <cyf4799@gmail.com>
2017-11-14 02:21:25 +0800

    Created a constructor for the Snake class.

--- Snatrix_Main/Snatrix_Main.ino
+++ Snatrix_Main/Snatrix_Main.ino
@@ -29,6 +29,14 @@ void loop() {
   if(currentTime - previousTime >= interval) {
     previousTime = currentTime; 
 
+
+    //Clear board
+    for(int row = 0; row < 16; row++) {
+      for(int column = 0; column < 16; column++) {
+        //Initialize entire board as off
+        board[row][column] = 0;
+      }
+    }
     //Print board to Serial
     for(int row = 0; row < 16; row++) {
       for(int column = 0; column < 16; column++) {
@@ -43,11 +51,28 @@ void loop() {
 //Snake Class
 class Snake 
 {
-  //2D array of int x and y coordinates of the snake 
+  //2D array of int row and column coordinates of the snake 
   int location[256][2]; 
   //Integer variable pointing to the tail of the snake (pointer to the location 2D array)
   int tail; 
   //Integer variable pointing to the head of the snake (pointer to the location 2D array) 
   int head;
+
+  //Constructor for Snake
+  Snake() {
+    //Set initial location of snake to be: (7, 6), (7, 7), (7, 8), (7, 9)
+    location[0][0] = 7;
+    location[0][1] = 6;
+    location[1][0] = 7;
+    location[1][1] = 7;
+    location[2][0] = 7;
+    location[2][1] = 8;
+    location[3][0] = 7;
+    location[3][1] = 9;
+
+    //Set head and tail pointers 
+    tail = 0;
+    head = 3;   
+  }
 };
 
